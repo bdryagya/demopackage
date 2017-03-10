435234cc51fd5cfa46e72a3fae357e2ff4d16c57
Yagya Chaudhary <mail@bdryagya.com.np>
2017-03-10 13:33:14 +0545

    Init

---
+++ src/Demo.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Demo Class
+ *
+ * @package  DemoPackage
+ * @author   "Yagya Chaudhary" <mail@bdryagya.com.np>
+ */
+namespace Bakhari\DemoPackage;
+
+class Demo
+{
+    public function run()
+    {
+        return "I'm Running!";
+    }
+}
